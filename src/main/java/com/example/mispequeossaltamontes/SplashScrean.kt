9838ff4738ce1c429ff38b3bb320c0dd700387ae
JuanNore <89522698+JuanNore@users.noreply.github.com>
2022-03-31 14:25:46 -0500

    Mis pequeños Saltamontes

---
+++ src/main/java/com/example/mispequeossaltamontes/SplashScrean.kt
@@ -0,0 +1,11 @@
+package com.example.mispequeossaltamontes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScrean : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screan)
+    }
+}
